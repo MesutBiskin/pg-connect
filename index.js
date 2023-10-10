594e107d625ebfe25319ad068d392d865d561cf8
Mesut Biskin <msbiskin@gmail.com>
2023-10-10 16:58:42 -0400

    Connected to Database and retreived all customers in table

---
+++ index.js
@@ -0,0 +1,24 @@
+// import tools from pg library.
+// import { Client } from 'pg'
+
+import pg from 'pg'
+const { Client } = pg
+
+// import connection string from credentials / secrets file
+import { connectionURI } from './creds.js'
+
+// create client connection to database
+const client = new Client({
+    connectionString:connectionURI,
+})
+
+// open connection
+await client.connect()
+
+// run query
+const results = await client.query('SELECT * FROM customers')
+
+console.table(results.rows)
+
+// close connection
+client.end
